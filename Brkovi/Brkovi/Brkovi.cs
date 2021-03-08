e978c4c210f01ba50bdb1f7cc19aecf5a1e598e1
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-03-08 09:17:42 +0100

    Add files via upload

---
+++ Brkovi/Brkovi/Brkovi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brkovi
+{
+    class Brkovi
+    {
+        string pod1, pod2, pod4, dodatak;
+        int pod3;
+
+        public string Pod1 { get => pod1; set => pod1 = value; }
+        public string Pod2 { get => pod2; set => pod2 = value; }
+        public string Pod4 { get => pod4; set => pod4 = value; }
+        public string Dodatak { get => dodatak; set => dodatak = value; }
+        public int Pod3 { get => pod3; set => pod3 = value; }
+
+        public void setPod1(string pod1)
+        {
+            this.Pod1 = pod1;
+        }
+
+        public string getPod1()
+        {
+            return this.Pod1;
+        }
+
+        public Brkovi(string pod1, string pod2, string pod4, int pod3)
+        {
+            this.pod1 = pod1;
+            this.pod2 = pod2;
+            this.pod3 = pod3;
+            this.pod4 = pod4;
+
+        }
+
+
+        public string Ispis()
+        {
+            string ispis = pod1 + "\t" + pod2 + "\t" + Convert.ToString(pod3) + "\t"
+                + pod4 + "\r\n";
+            return ispis;
+                
+        }
+
+        override public string ToString()
+        {
+            string ispis = pod1 + "\t\t" + pod2 + "\t\t"
+                + Convert.ToString(pod3) + "\r\n";
+            return ispis;
+        }
+
+
+
+    }
+}
---
+++ Brkovi/Brkovi/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Brkovi
+{
+    public partial class Form1 : Form
+    {
+        List<Brkovi> lista = new List<Brkovi>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUnos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Brkovi brk = new Brkovi(textBox1.Text, textBox2.Text, Convert.ToString(comboBox1.Text), Convert.ToInt32(textBox3.Text));
+
+                lista.Add(brk);
+
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+
+
+
+
+            }
+            catch (Exception greska)
+            {
+
+                MessageBox.Show("Pogrešan unos!\r\n" + greska.Message,
+                    "Greška", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnIspis_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Podatak 1" + "\tPodatak 2"
+                + "\tPodatak 3" + "\tPodatak 4" + "\tDodatak\r\n";
+
+
+            foreach (Brkovi brk in lista)
+            {
+                richTextBox1.AppendText(brk.ToString());
+            }
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+
+            foreach (Brkovi brk in lista)
+            {
+                if (brk.Pod4 == "M")
+                {
+                    brk.Dodatak = "Ima brkove.";
+                }
+                else
+                {
+                    brk.Dodatak = "Nema brkove";
+                }
+            }
+        }
+    }
+}
